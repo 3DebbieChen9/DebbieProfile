07c36fb98d19f0c1a76b7ca16bf03ec16e6bd90a
Debbie Chen <debbie8839@gmail.com>
2024-09-28 01:48:50 +0800

    ProfileUpdate

--- scripts/main.js
+++ scripts/main.js
@@ -9,36 +9,36 @@
 //     alert('Ouch! Stop poking me!');
 // }
 
-var myImage = document.querySelector('img');
-myImage.onclick = function() {
-    var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/WaiZui_Silhouette.PNG') {
-      myImage.setAttribute ('src','images/KillingEve.PNG');
-    } else {
-      myImage.setAttribute ('src','images/WaiZui_Silhouette.PNG');
-    }
-}
+// var myImage = document.querySelector('img');
+// myImage.onclick = function() {
+//     var mySrc = myImage.getAttribute('src');
+//     if(mySrc === 'images/WaiZui_Silhouette.PNG') {
+//       myImage.setAttribute ('src','images/KillingEve.PNG');
+//     } else {
+//       myImage.setAttribute ('src','images/WaiZui_Silhouette.PNG');
+//     }
+// }
 
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
+// var myButton = document.querySelector('button');
+// var myHeading = document.querySelector('h1');
 
-function setUserName() {
-    let myName = prompt('Please enter your name.');
-    if (!myName || myName === null) {
-        setUserName();
-    } else {
-        localStorage.setItem('name', myName);
-        myHeading.textContent = 'Wai Zui is cool, ' + myName;
-    }
-}
+// function setUserName() {
+//     let myName = prompt('Please enter your name.');
+//     if (!myName || myName === null) {
+//         setUserName();
+//     } else {
+//         localStorage.setItem('name', myName);
+//         myHeading.textContent = 'Wai Zui is cool, ' + myName;
+//     }
+// }
 
-if(!localStorage.getItem('name')) {
-    setUserName();
-} else {
-    let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Wai Zui is cool, ' + storedName;
-}
+// if(!localStorage.getItem('name')) {
+//     setUserName();
+// } else {
+//     let storedName = localStorage.getItem('name');
+//     myHeading.textContent = 'Wai Zui is cool, ' + storedName;
+// }
 
-myButton.onclick = function() {
-    setUserName();
-}
+// myButton.onclick = function() {
+//     setUserName();
+// }
